63d34326ec2ca43e8f39d8010243f9ebcafc64af
nahidalamgh <nahidalamgh@yahoo.com>
2021-03-17 09:50:19 +0100

    Home fragment_home.xml HomeFragment

--- app/src/main/java/com/example/dietappproject/hometab/HomeFragment.java
+++ app/src/main/java/com/example/dietappproject/hometab/HomeFragment.java
@@ -1,21 +1,62 @@
 package com.example.dietappproject.hometab;
 
+
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
+
 import com.example.dietappproject.R;
+import com.example.dietappproject.dbobject.User;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Date;
+import java.util.Locale;
+import android.icu.text.SimpleDateFormat;
 
 public class HomeFragment extends Fragment {
+
+    private FirebaseFirestore db;
+    private String userId;
+
+    private TextView  dateTimeDisplay;
+    private TextView textViewhome;
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_home, container, false);
+
+        dateTimeDisplay = v.findViewById(R.id.dateTimeDisplay);
+        textViewhome = v.findViewById(R.id.textViewhome);
+
+        String date = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
+        dateTimeDisplay.setText(date);
+
+        db = FirebaseFirestore.getInstance();
+        userId = FirebaseAuth.getInstance().getUid();
+        db.collection("Users").document(userId)
+                .get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                User user = documentSnapshot.toObject(User.class);
+                user.setDocumentId(documentSnapshot.getId());
+                textViewhome.setText("Welcome " + String.valueOf(user.getName()));
+
+            }});
         return v;
+
     }
 }
--- app/src/main/java/com/example/dietappproject/mealtab/MealTodayFragment.java
+++ app/src/main/java/com/example/dietappproject/mealtab/MealTodayFragment.java
@@ -1,6 +1,6 @@
 package com.example.dietappproject.mealtab;
 
-import android.os.Bundle;
+import  android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
