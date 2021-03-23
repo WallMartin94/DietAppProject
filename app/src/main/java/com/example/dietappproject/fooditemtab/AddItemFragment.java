158faaed6b15a9975b126de6b089b215384c4fb7
WallMartin94 <57918288+WallMartin94@users.noreply.github.com>
2021-03-23 09:30:49 +0100

    Changed scan button with an icon instead

--- app/src/main/java/com/example/dietappproject/fooditemtab/AddItemFragment.java
+++ app/src/main/java/com/example/dietappproject/fooditemtab/AddItemFragment.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -32,7 +33,7 @@ public class AddItemFragment extends Fragment implements BarcodeFragment.CameraL
     private static final String TAG = "AddItemFragment";
 
     View view;
-    Button scanButton;
+    ImageButton scanButton;
     EditText barCodeTextView;
     EditText nameTextView;
     EditText proteinTextView;
